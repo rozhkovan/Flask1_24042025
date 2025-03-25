29834b52b2a288ed4928aa1b0a9256b9cda69633
Andrey Rozhkov <arozhkov@list.ru>
2025-03-25 17:39:06 +0300

    Add sqlite examples

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from random import choice
 from typing import Any
+from http import HTTPStatus
 
 app = Flask(__name__)
 app.json.ensure_ascii = False
@@ -10,6 +11,9 @@ about_me = {
     "sufname": "Рожков",
     "email": "arozhkov@list.ru"
 }
+
+QUOTES_KEY = set(('author', 'text', 'rate'))
+RATE_RANGE = range(1,6)
 
 quotes = [
    {
@@ -52,53 +56,60 @@ def get_index_by_id(id: int) -> int:
 
 @app.route("/")
 def hello_world() -> dict:
-    return jsonify(data = "Hello, World!")
+    return jsonify(data = "Hello, World!"), HTTPStatus.OK
 
 
 @app.route("/about")
 def about() -> dict:
-    return jsonify(about_me)
+    return jsonify(about_me), HTTPStatus.OK
 
 
 @app.route("/quotes")
 def get_quotes() -> list[dict[str, Any]]: 
-    return jsonify(quotes)
+    return jsonify(quotes), HTTPStatus.OK
 
 
 @app.route("/quotes", methods=['POST'])
 def create_quote() -> dict:
-    q = dict(request.json)
-    q['id'] = get_new_id()
-    new_rate = q.get('rate')
-    if new_rate:
-        if new_rate not in range(1,6):
-            q['rate'] = 1
-    else:
-        q['rate'] = 1 
-    quotes.append(q)
-    return jsonify(q), 200
+    new_data = dict(request.json)
+    wrong_keys = set(new_data.keys()) - QUOTES_KEY
+    if wrong_keys:
+        return jsonify(error = f"Недопустимый ключ {wrong_keys}"), HTTPStatus.BAD_REQUEST
+    new_data['id'] = get_new_id()
+    new_rate = new_data.get('rate')
+    if new_rate is None or new_rate not in RATE_RANGE:
+        # if new_rate not in RATE_RANGE:
+        new_data['rate'] = min(RATE_RANGE)
+    # else:
+    #     new_data['rate'] = 1 
+    quotes.append(new_data)
+    return jsonify(new_data), HTTPStatus.OK
 
 
 @app.route("/quotes/<int:id>")
 def get_quote(id : int) -> dict:
     index = get_index_by_id(id) 
     if index:
-        return jsonify(quotes[index])
-    return jsonify(error = f"Quote with id={id} not found"), 404
+        return jsonify(quotes[index]), HTTPStatus.OK
+    return jsonify(error = f"Quote with id={id} not found"), HTTPStatus.NOT_FOUND
 
 
 @app.route("/quotes/<int:id>", methods=['PUT'])
 def edit_quote(id: int) -> dict:
     index = get_index_by_id(id)
     if index:
-        new = dict(request.json)
-        new_rate = new.get('rate')
-        if new_rate not in range(1,6):
-            new['rate'] = quotes[index]['rate']
-        for key in new:
-            quotes[index][key] = new[key]
-        return jsonify(quotes[index]), 200
-    return jsonify(error = f"Quote with id={id} not found"), 404
+        new_data = dict(request.json)
+        wrong_keys = set(new_data.keys()) - QUOTES_KEY
+        if wrong_keys:
+            return jsonify(error = f"Недопустимый ключ {wrong_keys}"), HTTPStatus.BAD_REQUEST
+        new_rate = new_data.get('rate')
+        if new_rate not in RATE_RANGE:
+            new_data.pop['rate']
+        quotes[index].update(new_data)
+        # for key in new_data:
+        #     quotes[index][key] = new_data[key]
+        return jsonify(quotes[index]), HTTPStatus.OK
+    return jsonify(error = f"Quote with id={id} not found"), HTTPStatus.NOT_FOUND
 
 
 @app.route("/quotes/<int:id>", methods=['DELETE'])
@@ -106,18 +117,18 @@ def delete_quote(id: int):
     index = get_index_by_id(id)
     if index:
         quotes.pop(index)
-        return jsonify(message = f"Quote with id={id} is deleted."), 200
-    return jsonify(error = f"Quote with id={id} not found"), 404
+        return jsonify(message = f"Quote with id={id} is deleted."), HTTPStatus.OK
+    return jsonify(error = f"Quote with id={id} not found"), HTTPStatus.NOT_FOUND
 
 
 @app.route("/quotes/count")
 def quotes_count():
-    return jsonify(count = len(quotes))
+    return jsonify(count = len(quotes)), HTTPStatus.OK
 
 
 @app.route("/quotes/random")
 def random_quote() -> dict:
-    return jsonify(choice(quotes))
+    return jsonify(choice(quotes)) ,HTTPStatus.OK
 
 
 @app.route("/quotes/filter", methods=['GET'])
@@ -126,10 +137,10 @@ def filtered_quotes() -> list[dict]:
     rate_filter = args.get('rate')
     if rate_filter:
         args['rate'] = int(rate_filter)
-    result = quotes
+    result = quotes.copy()
     for key in args:
         result = list(filter(lambda x: x[key] == args[key], result))
-    return jsonify(result)
+    return jsonify(result), HTTPStatus.OK
 
 
 if __name__ == "__main__":
