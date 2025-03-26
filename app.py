fac9bdb823f55a21bee51b76b4e122cb24304fce
Andrey Rozhkov <arozhkov@list.ru>
2025-03-27 00:49:13 +0300

    Fixed some URL

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ class AuthorModel(db.Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[int] = mapped_column(String(32), index= True, unique=True)
-    quotes: Mapped[list['QuoteModel']] = relationship( back_populates='author', lazy='dynamic')
+    quotes: Mapped[list['QuoteModel']] = relationship(back_populates='author', lazy='dynamic', cascade="all,delete-orphan")
     
     def __init__(self, name):
         self.name = name
@@ -68,10 +68,10 @@ class QuoteModel(db.Model):
     def to_dict(self):
         return {
             "id": self.id,
+            "author_id": self.author_id,
             # "author": self.author,
             "text": self.text
         }
-    
 
 
 @app.errorhandler(HTTPException)
@@ -79,17 +79,55 @@ def handle_exception(e):
     return jsonify(message = e.description), e.code
 
 
-@app.route("/quotes")
-def get_quotes() -> list[dict[str, Any]]: 
-    # quotes_db = db.session.scalars(db.select(QuoteModel)).all()
-    quotes_db = db.session.execute(db.select(QuoteModel)).scalars()
-    quotes = []
-    for quote_db in quotes_db:
-        quotes.append(quote_db.to_dict())        
-    return jsonify(quotes), HTTPStatus.OK
+@app.route("/authors/<int:author_id>")
+def get_author(author_id): 
+    author = db.session.get(AuthorModel, author_id)
+    if author:
+        return jsonify(author = author.to_dict()), HTTPStatus.OK
+    return jsonify(error = f"Author with id={author_id} not found"), HTTPStatus.NOT_FOUND
 
 
-@app.route("/author/<int:author_id>/quotes")
+@app.route("/authors")
+def get_authors() -> list[dict[str, Any]]: 
+    authors_db = db.session.execute(db.select(AuthorModel)).scalars()
+    authors = []
+    for author_db in authors_db:
+        authors.append(author_db.to_dict())        
+    return jsonify(authors), HTTPStatus.OK
+
+
+@app.route("/authors", methods=["POST"])
+def create_author():
+    author_data = request.json
+    author = AuthorModel(author_data["name"])
+    db.session.add(author)
+    db.session.commit()
+    return author.to_dict(), HTTPStatus.CREATED
+
+
+# @app.route("/authors/<int:author_id>", methods=["PUT"])
+# def edit_author(author_id):
+#     author_data = request.json
+#     author = db.session.get(AuthorModel, author_id)
+#     # author = AuthorModel(author_data["name"])
+
+#     print(**author_data)
+#     # db.session.add(author)
+#     # db.session.commit()
+#     return author.to_dict(), HTTPStatus.CREATED
+
+
+@app.route("/authors/<int:author_id>", methods=["DELETE"])
+def delete_author(author_id):
+    author = db.session.get(AuthorModel, author_id)
+    if author:
+        db.session.delete(author)
+        db.session.commit()
+        return jsonify(message = f"Author with id={author_id} deleted."), HTTPStatus.OK
+    return jsonify(message = f"Author with id={author_id} not found"), HTTPStatus.NOT_FOUND
+
+
+@app.route("/authors/<int:author_id>/quotes")
 def get_author_quotes(author_id): 
     author = db.session.get(AuthorModel, author_id)
     quotes = []
@@ -98,65 +136,88 @@ def get_author_quotes(author_id):
     return jsonify(author = author.to_dict(), quotes = quotes), HTTPStatus.OK
 
 
-@app.route("/quotes", methods=['POST'])
-def create_quote() -> dict:
-    # Проверка данных
-    new_data = dict(request.json)
-    wrong_keys = set(new_data.keys()) - QUOTES_KEYS
-    if wrong_keys:
-        return jsonify(error = f"Wrong keys {wrong_keys}"), HTTPStatus.BAD_REQUEST
-    new_rate = new_data.get('rate')
-    if new_rate is None or new_rate not in RATE_RANGE:
-        new_data['rate'] = min(RATE_RANGE)
-
-    # Вставка записи
-    # quote = QuoteModel(new_data['author'], new_data['text'], new_data['rate'])
-    quote_db = QuoteModel(new_data['author'], new_data['text'])
-    db.session.add(quote_db)
-    db.session.commit()
-    return jsonify(quote_db.to_dict()), HTTPStatus.OK
+@app.route("/authors/<int:author_id>/quotes", methods=["POST"])
+def create_author_quote(author_id: int):
+    author = db.session.get(AuthorModel, author_id)
+    if author:
+        new_data = request.json
+        quote = QuoteModel(author, new_data["text"])
+        db.session.add(quote)
+        db.session.commit()
+        return quote.to_dict(), HTTPStatus.CREATED
+    return jsonify(message = f"Author with id={author_id} not found"), HTTPStatus.NOT_FOUND
 
 
-@app.route("/quotes/<int:id>")
-def get_quote(id : int) -> dict:
-    quote_db = db.session.execute(db.select(QuoteModel).filter_by(id=id)).scalar_one_or_none()
-    if quote_db:
+@app.route("/quotes")
+def get_quotes() -> list[dict[str, Any]]: 
+    quotes_db = db.session.execute(db.select(QuoteModel)).scalars()
+    quotes = []
+    for quote_db in quotes_db:
+        quotes.append(quote_db.to_dict())        
+    return jsonify(quotes), HTTPStatus.OK
+
+
+# @app.route("/quotes", methods=['POST'])
+# def create_quote() -> dict:
+#     # Проверка данных
+#     new_data = dict(request.json)
+#     wrong_keys = set(new_data.keys()) - QUOTES_KEYS
+#     if wrong_keys:
+#         return jsonify(error = f"Wrong keys {wrong_keys}"), HTTPStatus.BAD_REQUEST
+#     new_rate = new_data.get('rate')
+#     if new_rate is None or new_rate not in RATE_RANGE:
+#         new_data['rate'] = min(RATE_RANGE)
+
+#     # Вставка записи
+#     # quote = QuoteModel(new_data['author'], new_data['text'], new_data['rate'])
+#     quote_db = QuoteModel(new_data['author'], new_data['text'])
+#     db.session.add(quote_db)
+#     db.session.commit()
+#     return jsonify(quote_db.to_dict()), HTTPStatus.OK
+
+
+@app.route("/quotes/<int:quote_id>")
+def get_quote(quote_id : int) -> dict:
+    # quote_db = db.session.execute(db.select(QuoteModel).filter_by(id=id)).scalar_one_or_none()
+    quote = db.session.get(QuoteModel, quote_id)
+    if quote:
         # quote = qoute_db.to_dict()
-        return jsonify(quote_db.to_dict()), HTTPStatus.OK
-    return jsonify(error = f"Quote with id={id} not found"), HTTPStatus.NOT_FOUND
+        return jsonify(quote.to_dict()), HTTPStatus.OK
+    return jsonify(error = f"Quote with id={quote_id} not found"), HTTPStatus.NOT_FOUND
 
 
-@app.route("/quotes/<int:id>", methods=['PUT'])
-def edit_quote(id: int) -> dict:
-    # Проверка данных
-    new_data = dict(request.json)
-    wrong_keys = set(new_data.keys()) - QUOTES_KEYS
-    if wrong_keys:
-        return jsonify(error = f"Wrong keys {wrong_keys}"), HTTPStatus.BAD_REQUEST
-    new_rate = new_data.get('rate')
-    if new_rate and new_rate not in RATE_RANGE:
-        new_data.pop['rate']
+# @app.route("/quotes/<int:quote_id>", methods=['PUT'])
+# def edit_quote(quote_id: int) -> dict:
+#     # Проверка данных
+#     new_data = dict(request.json)
+#     wrong_keys = set(new_data.keys()) - QUOTES_KEYS
+#     if wrong_keys:
+#         return jsonify(error = f"Wrong keys {wrong_keys}"), HTTPStatus.BAD_REQUEST
+#     new_rate = new_data.get('rate')
+#     if new_rate and new_rate not in RATE_RANGE:
+#         new_data.pop['rate']
 
-    # Обновление записи
-    quote_db = db.session.execute(db.select(QuoteModel).filter_by(id=id)).scalar_one_or_none()
-    if quote_db:
-        quote = quote_db.to_dict()
-        quote.update(new_data)
-        quote_db.author = quote['author']
-        quote_db.text = quote['text']
+#     # Обновление записи
+#     quote_db = db.session.execute(db.select(QuoteModel).filter_by(id=quote_id)).scalar_one_or_none()
+#     if quote_db:
+#         quote = quote_db.to_dict()
+#         quote.update(new_data)
+#         quote_db.author = quote['author']
+#         quote_db.text = quote['text']
+#         db.session.commit()
+#         return jsonify(quote_db.to_dict()), HTTPStatus.OK
+#     return jsonify(error = f"Quote with id={quote_id} not found"), HTTPStatus.NOT_FOUND
+
+
+@app.route("/quotes/<int:quote_id>", methods=['DELETE'])
+def delete_quote(quote_id: int):
+    # quote_db = db.session.execute(db.select(QuoteModel).filter_by(id=quote_id)).scalar_one_or_none()
+    quote = db.session.get(QuoteModel, quote_id)
+    if quote:
+        db.session.delete(quote)
         db.session.commit()
-        return jsonify(quote_db.to_dict()), HTTPStatus.OK
-    return jsonify(error = f"Quote with id={id} not found"), HTTPStatus.NOT_FOUND
-
-
-@app.route("/quotes/<int:id>", methods=['DELETE'])
-def delete_quote(id: int):
-    quote_db = db.session.execute(db.select(QuoteModel).filter_by(id=id)).scalar_one_or_none()
-    if quote_db:
-        db.session.delete(quote_db)
-        db.session.commit()
-        return jsonify(message = f"Quote with id={id} deleted."), HTTPStatus.OK
-    return jsonify(error = f"Quote with id={id} not found"), HTTPStatus.NOT_FOUND
+        return jsonify(message = f"Quote with id={quote_id} deleted."), HTTPStatus.OK
+    return jsonify(error = f"Quote with id={quote_id} not found"), HTTPStatus.NOT_FOUND
 
 
 # @app.route("/quotes/count")
